b1842f6ba5fccfa355f7953196b731b107584613
bruno <bruno.bueno@accountfy.com>
2023-10-27 15:55:13 -0300

    projetinho para apresentar documentação da wcag

---
+++ main.js
@@ -0,0 +1,76 @@
+const sections = [
+  "idioma-da-pagina",
+  "nome_funcao_valor",
+  "rotulo-no-nome-acessivel",
+  "identificar-o-obejtivo",
+];
+
+window.onload = onInit();
+
+function onInit() {
+  setSection();
+  checkPreviousButton();
+  checkNextButton();
+}
+
+function showPreviousSection() {
+  hideSection(getCurrentSection());
+  showSection(getPreviousSection());
+  checkPreviousButton();
+  checkNextButton();
+  saveSection();
+}
+
+function showNextSection() {
+  hideSection(getCurrentSection());
+  showSection(getNextSection());
+  checkPreviousButton();
+  checkNextButton();
+  saveSection();
+}
+
+function getCurrentSection() {
+  return document.getElementById(sections[currentSectionIndex]);
+}
+
+function getNextSection() {
+  currentSectionIndex += 1;
+  return getCurrentSection();
+}
+
+function getPreviousSection() {
+  currentSectionIndex -= 1;
+  return getCurrentSection();
+}
+
+function showSection(section) {
+  section.classList.remove("hidden");
+}
+
+function hideSection(section) {
+  section.classList.add("hidden");
+}
+
+function checkPreviousButton() {
+  const previousBtn = document.getElementById("previous");
+  previousBtn.disabled = currentSectionIndex === 0;
+}
+
+function checkNextButton() {
+  const nextBtn = document.getElementById("next");
+  nextBtn.disabled = currentSectionIndex === sections.length - 1;
+}
+
+function saveSection() {
+  window.localStorage.setItem("currentSection", currentSectionIndex);
+}
+
+function setSection() {
+  currentSectionIndex =
+    Number(window.localStorage.getItem("currentSection")) || 0;
+  showSection(getCurrentSection());
+}
+
+function changeDocumentLang(lang) {
+  document.documentElement.setAttribute("lang", lang);
+}
